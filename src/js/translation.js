2a40c78f89db7185a142f4fed6cdc6fcdea787d7
杨永鑫 <3271310698@qq.com>
2025-06-01 22:26:46 +0800

    first commit

---
+++ src/js/translation.js
@@ -0,0 +1,246 @@
+const TranslationData = [
+  [`Overview`, `概览`],
+  [`Repositories`, `仓库`],
+  [`Projects`, `项目`],
+  [`Packages`, `包`],
+  [`Stars`, `收藏`],
+
+  [`Sort`, `排序`],
+  [`Sort by`, `排序方式`],
+  [`Lists`, `列表 `],
+  [`✨ Inspiration`, `✨ 灵感`],
+  [`🚀 My stack`, `🚀 我的技术栈`],
+  [`🔮 Future ideas`, `🔮 未来想法`],
+  [`Search`, `搜索`],
+
+  [`Create new...`, `创建新...`],
+  [`New repository`, `新建仓库`],
+  [`Import repository`, `导入仓库`],
+  [`New codespace`, `新建代码空间`],
+  [`New gist`, `新建代码片段`],
+  [`New organization`, `新建组织`],
+  [`New project`, `新建项目`],
+
+  [`Account switcher`, `账户切换器`],
+  [`Close`, `关闭`],
+  [`Your profile`, `你的个人资料`],
+  [`Your repositories`, `你的仓库`],
+  [`Your Copilot`, `你的 Copilot`],
+  [`Your projects`, `你的项目`],
+  [`Your stars`, `你的收藏`],
+  [`Your gists`, `你的代码片段`],
+  [`Your organizations`, `你的组织`],
+  [`Your enterprises`, `你的企业`],
+  [`Your sponsors`, `你的赞助者`],
+  [`Try Enterprise`, `试用企业版`],
+  [`Free`, `免费`],
+  [`Feature preview`, `功能预览`],
+  [`Settings`, `设置`],
+  [`GitHub Website`, `GitHub 网站`],
+  [`GitHub Docs`, `GitHub 文档`],
+  [`GitHub Support`, `GitHub 支持`],
+  [`GitHub Community`, `GitHub 社区`],
+  [`Sign out`, `登出`],
+
+  // settings/profile
+  [`Your personal account`, `你的个人账户`],
+  [`Switch settings context`, `切换设置上下文`],
+  [`Go to your personal profile`, `前往你的个人资料`],
+  [`Public profile`, `公开资料`],
+  [`Profile picture`, `个人资料图片`],
+  [`Edit`, `编辑`],
+  [`Name`, `姓名`],
+  [`Your name may appear around GitHub where you contribute or are mentioned. You can remove it at any time.`, `你的姓名可能会出现在你贡献或被提及的 GitHub 上。你可以随时删除它。`],
+  [`Public email`, `公开邮箱`],
+  [`Select a verified email to display`, `选择一个已验证的邮箱进行显示`],
+  [`You have set your email address to private. To toggle email privacy, go to`, `你已将你的邮箱地址设置为私密。要切换邮箱隐私，请前往 `],
+  [`email settings`, `邮箱设置`],
+  [`and uncheck "Keep my email address private."`, ` 并取消选中“保持我的邮箱地址私密”。`],
+  [`Bio`, `简介`],
+  [`Tell us a little bit about yourself`, `告诉我们一些关于你自己的事情`],
+  [`You can`, `你可以 `],
+  [`other users and organizations to link to them.`, ` 其他用户和组织可以链接到它们。`],
+  [`Pronouns`, `代词`],
+  [`Don't specify`, `不指定`],
+  [`they/them`, `他们/她们`],
+  [`she/her`, `她/她`],
+  [`he/him`, `他/他`],
+  [`Custom`, `自定义`],
+  [`URL`, `网址`],
+  [`ORCID provides a persistent identifier - an ORCID iD - that distinguishes you from other researchers. Learn more at`, `ORCID 提供了一个持久的标识符 - ORCID iD - 用于区分你和其他研究人员。了解更多信息请访问 `],
+  [`Connect your ORCID iD`, `连接你的 ORCID iD`],
+  [`Social accounts`, `社交账户`],
+  [`Link to social profile 1`, `链接到社交资料 1`],
+  [`Link to social profile 2`, `链接到社交资料 2`],
+  [`Link to social profile 3`, `链接到社交资料 3`],
+  [`Link to social profile 4`, `链接到社交资料 4`],
+  [`Company`, `公司`],
+  [`your company’s GitHub organization to link it.`, ` 你的公司的 GitHub 组织以链接它。`],
+  [`Location`, `位置`],
+  [`Display current local time`, `显示当前本地时间`],
+  [`Other users will see the time difference from their local time.`, `其他用户将看到与他们本地时间的时差。`],
+  [`Time zone`, `时区`],
+  [`All of the fields on this page are optional and can be deleted at any time, and by filling them out, you're giving us consent to share this data wherever your user profile appears. Please see our`, `此页面上的所有字段都是可选的，可以随时删除，通过填写这些字段，你同意我们在你的用户资料出现的任何地方共享这些数据。请参阅我们的 `],
+  [`to learn more about how we use this information.`, ` 以了解我们如何使用这些信息。`],
+  [`Update profile`, `更新资料`],
+  [`Contributions & activity`, `贡献与活动`],
+  [`Make profile private and hide activity`, `将个人资料设为私密并隐藏活动`],
+  [`Enabling this will hide your contributions and activity from your GitHub profile and from social features like followers, stars, feeds, leaderboards and releases.`, `启用此选项将隐藏你的贡献和活动，无法在你的 GitHub 个人资料以及关注者、收藏、动态、排行榜和版本等社交功能中显示。`],
+  [`Include private contributions on my profile`, `在我的个人资料中包含私有贡献`],
+  [`Your contribution graph, achievements, and activity overview will show your private contributions without revealing any repository or organization information.`, `你的贡献图表、成就和活动概览将显示你的私有贡献，而不会透露任何仓库或组织信息。 `],
+  [`Read more`, `阅读更多`],
+  [`Update preferences`, `更新偏好设置`],
+  [`Profile settings`, `个人资料设置`],
+  [`Show Achievements on my profile`, `在我的个人资料上显示成就`],
+  [`Your achievements will be shown on your profile.`, `你的成就将显示在你的个人资料上。`],
+  [`GitHub Developer Program`, `GitHub 开发者计划`],
+  [`Building an application, service, or tool that integrates with GitHub?`, `正在构建一个与 GitHub 集成的应用程序、服务或工具吗？`],
+  [`Join the GitHub Developer Program`, `加入 GitHub 开发者计划`],
+  [`, or read more about it at our`, `，或在我们的 `],
+  [`GitHub developer program`, `GitHub 开发者计划`],
+  [`Jobs profile`, `职位资料`],
+  [`Available for hire`, `可供雇佣`],
+  [`Save jobs profile`, `保存职位资料`],
+  [`Trending settings`, `趋势设置`],
+  [`Preferred spoken language`, `首选语言`],
+  [`We'll use this language preference to filter the trending repository lists on`, `我们将使用此语言偏好来过滤 `],
+  [`Explore`, `探索`],
+  [`our`, ` 我们的 `],
+  [`Trending Repositories`, `趋势仓库`],
+  [`page.`, ` 页面。`],
+  [`Save Trending settings`, `保存趋势设置`],
+
+  // settings/admin
+  [`Account`, `账户`],
+  [`Change username`, `更改用户名`],
+  [`Changing your username can have`, `更改用户名可能会有 `],
+  [`unintended side effects`, `意外的副作用`],
+  [`Looking to manage account security settings? You can find them in the`, ` 想要管理账户安全设置？你可以在 `],
+  [`Password and authentication`, `密码和身份验证`],
+  [`Link Patreon account`, `链接 Patreon 账户`],
+  [`Connect a Patreon account for`, `连接一个 Patreon 账户以 `],
+  [`to sponsor maintainers with. Get recognition on GitHub for sponsorships made on Patreon when the sponsored person has linked Patreon and GitHub, too, and has a public GitHub Sponsors profile.`, ` 用于赞助维护者。当被赞助的人也链接了 Patreon 和 GitHub，并且有一个公开的 GitHub Sponsors 个人资料时，可以在 GitHub 上获得对 Patreon 赞助的认可。`],
+  [`Connect with Patreon`, `连接到 Patreon`],
+  [`Export account data`, `导出账户数据`],
+  [`Export all repositories and profile metadata for`, `导出所有仓库和个人资料元数据以供 `],
+  [`. Exports will be available for 7 days.`, `。导出将在 7 天内可用。`],
+  [`Start export`, `开始导出`],
+  [`Successor settings`, `继任者设置`],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+  [``],
+];
+
+const MutationObserverConfig = {
+  childList: true,
+  subtree: true,
+  attributeFilter: ["data-label"],
+  characterData: true,
+};
+
+const observer = new MutationObserver(function (mutations) {
+  const treeWalker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_ALL,
+    {
+      acceptNode: function (node) {
+        if (
+          node.nodeType === 3 ||
+          (node.hasAttribute &&
+            (node.hasAttribute("data-label") ||
+              node.hasAttribute("placeholder") ||
+              node.hasAttribute("value")))
+        ) {
+          return NodeFilter.FILTER_ACCEPT;
+        } else {
+          return NodeFilter.FILTER_SKIP;
+        }
+      },
+    },
+    false
+  );
+  let dataMap = new Map();
+  TranslationData.forEach(([key, val]) => {
+    if (key && !dataMap.has(key)) {
+      dataMap.set(key, val);
+    }
+  });
+  let currentNode = treeWalker.currentNode;
+  while (currentNode) {
+    if (currentNode.nodeType === 3) {
+      let key1 = currentNode.textContent
+        .replace(/^\s*|\s*$/g, "")
+        .replace(/\s+/g, " ");
+      if (dataMap.has(key1)) currentNode.textContent = dataMap.get(key1);
+    } else {
+      let key2 = currentNode.getAttribute("data-label");
+      if (key2 && dataMap.has(key2))
+        currentNode.setAttribute("data-label", dataMap.get(key2));
+      let key3 = currentNode.getAttribute("placeholder") || "";
+      if ((key3 = key3.trim())) {
+        if (dataMap.has(key3))
+          currentNode.setAttribute("placeholder", dataMap.get(key3));
+      }
+      let key4 = currentNode.getAttribute("value");
+      if (currentNode.tagName == "INPUT" && dataMap.has(key4)) {
+        currentNode.setAttribute("value", dataMap.get(key4));
+        let key5 = currentNode.getAttribute("data-signin-label");
+        let key6 = currentNode.getAttribute("data-disable-with");
+        currentNode.setAttribute("data-signin-label", dataMap.get(key5));
+        currentNode.setAttribute("data-disable-with", dataMap.get(key6));
+      }
+    }
+    currentNode = treeWalker.nextNode();
+  }
+});
+
+observer.observe(document.body, MutationObserverConfig);
